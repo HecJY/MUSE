144a418d54af8d9c79ff73377920e287a81044c7
Jiaxing Yang <yang1274@ecegrid-thin6.ecn.purdue.edu>
2019-04-15 19:49:13 -0400

    finished debugging the wave generation function

--- mini_project/src/wave.c
+++ mini_project/src/wave.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include "freq.h"
 #include <stdio.h>
+#define M_PI 3.141592
 
-
+int waveform[50];
 int *waveform_gen(int key_index);
 
 
-
-int *waveform_gen(key_index){
+int *waveform_gen(int key_index){
+	float offset = 2048.0;
+	float amp = 500;
 	int n = 50;
-	int waveform[50];
 	float freq = freqtable[key_index];
-	for(int i; i< n; i++){
-		float point = sin(i*2*M_PI*freq);
+	for(int i=0; i< n; i++){
+		float point =offset +amp* sin(i*2*M_PI*freq);
 		waveform[i] = (int) point;
 	}
 
@@ -22,9 +23,10 @@ int *waveform_gen(key_index){
 }
 
 int main () {
-	int waveform1 = waveform_gen(1);
+	int *waveform1;
+	waveform1 = waveform_gen(1);
 	int loop;
-	for(loop = 0; loop < 10; loop++){
+	for(loop = 0; loop < 50; loop++){
 
 	   printf("%d ", waveform1[loop]);
 	}
